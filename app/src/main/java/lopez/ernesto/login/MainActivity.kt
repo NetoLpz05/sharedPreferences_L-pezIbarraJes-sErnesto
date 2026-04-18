bdd24cbe13f3786c65eaa99b0698094bc436940c
Ernesto López <jesus.lopez252663@potros.itson.edu.mx>
2026-04-17 20:41:15 -0700

    Update MainActivity.kt

--- app/src/main/java/lopez/ernesto/login/MainActivity.kt
+++ app/src/main/java/lopez/ernesto/login/MainActivity.kt
@@ -10,8 +10,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.unit.dp
 import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.*
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import lopez.ernesto.login.screen.*
